94e3a7f3b5693112b8afc55790dd392d6dbc4b92
Thomas CARTON <tom.carton@gmail.com>
2021-10-01 17:07:10 +0200

    Set some constants

--- Arduino/K2000/K2000.ino
+++ Arduino/K2000/K2000.ino
@@ -1,23 +1,24 @@
 // K2000
 //
 
-int LedCount = 6;
-int LedPins[] = {3, 5, 6, 9, 10, 11, 12};
+const int kLedCount = 6;
+const int kLedPins[] = {3, 5, 6, 9, 10, 11, 12};
 
 int ledIntensity[] = {0, 0, 0, 0, 0, 0};
+
 
 void setup() {
   Serial.begin(9600);
   
-  for(int i = 0; i < LedCount; ++i) {
-    pinMode(LedPins[i], OUTPUT);
-    digitalWrite(LedPins[i], LOW);
+  for(int i = 0; i < kLedCount; ++i) {
+    pinMode(kLedPins[i], OUTPUT);
+    digitalWrite(kLedPins[i], LOW);
   }
 }
 
 void apply() {
-  for(int i = 0; i < LedCount; ++i) {
-    analogWrite(LedPins[i], ledIntensity[i]);
+  for(int i = 0; i < kLedCount; ++i) {
+    analogWrite(kLedPins[i], ledIntensity[i]);
     ledIntensity[i] -= 25;
     if (ledIntensity[i] <= 0)
       ledIntensity[i] = 0; 
