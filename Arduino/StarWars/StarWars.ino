44917be0added923945b4c24babc27a8efbc073b
Thomas CARTON <tom.carton@gmail.com>
2021-10-04 08:27:43 +0200

    Initial StarWars tune Fix note issue in the provided sample

---
+++ Arduino/StarWars/StarWars.ino
@@ -0,0 +1,81 @@
+// StarWars
+//
+
+const int buzzerPin = 9;
+const int redLedPin = 10;
+
+void setup()
+{
+  pinMode(buzzerPin, OUTPUT);
+}
+
+void playNote(int note, int duration)
+{
+  digitalWrite(redLedPin, HIGH);
+  tone(buzzerPin, note, duration);
+
+  delay(duration);
+  digitalWrite(redLedPin, LOW);
+
+  delay(50);
+}
+
+void loop()
+{
+  playNote(440, 10 * 50); // A4
+  playNote(440, 10 * 50); // A4
+  playNote(440, 10 * 50); // A4
+  playNote(349, 7 * 50); // F4
+  playNote(523, 3 * 50); // C5
+  playNote(440, 10 * 50); // A4
+  playNote(349, 7 * 50); // F4
+  playNote(523, 3 * 50); // C5
+  playNote(440, 13 * 50); // A4
+ 
+  delay(500);
+ 
+  playNote(659, 10 * 50); // E5
+  playNote(659, 10 * 50); // E5
+  playNote(659, 10 * 50); // E5
+  playNote(698, 7 * 50); // F5
+  playNote(523, 3 * 50); // C5
+  playNote(415, 10 * 50); // G#4
+  playNote(349, 7 * 50); // F4
+  playNote(523, 3 * 50); // C5
+  playNote(440, 13 * 50); // A4
+ 
+  delay(500);
+
+  playNote(880, 500); // A5
+  playNote(440, 300); // A4
+  playNote(440, 150); // A4
+  playNote(880, 500); // A5
+  playNote(830, 325); // G#5
+  playNote(784, 175);  // G5
+  playNote(740, 125); // F#5
+  playNote(698, 125);  // F5
+  playNote(740, 250); // F#5
+ 
+  delay(325);
+ 
+  playNote(466, 250); // A#4
+  playNote(622, 500); // D#5
+  playNote(587, 325); // D5
+  playNote(554, 175); // C#5
+  playNote(523, 125); // C5
+  playNote(482, 125); // B4
+  playNote(523, 250); // C5
+ 
+  delay(350);
+  
+  playNote(349, 250); // F4
+  playNote(415, 500); // G#4
+  playNote(349, 350); // F4
+  playNote(440, 125); // A4
+  playNote(523, 500); // C5
+  playNote(440, 375); // A4
+  playNote(523, 125); // C5
+  playNote(659, 650); // E5
+ 
+  delay(500);
+}
