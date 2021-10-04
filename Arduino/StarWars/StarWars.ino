85b393b2a26445dab083a763f4b232102a299b4d
Thomas CARTON <tom.carton@gmail.com>
2021-10-04 11:55:49 +0200

    Set the song partition in a 'soundtracker' mode

--- Arduino/StarWars/StarWars.ino
+++ Arduino/StarWars/StarWars.ino
@@ -1,6 +1,8 @@
 // StarWars
 //
 
+const int kDebug = 0;
+
 const int kBuzzerPin = 9;
 const int kFirstLedPin = 10;
 const int kLedPinCount = 3;
@@ -9,6 +11,12 @@ int noteIndex = 0;
 
 void setup()
 {
+  if (kDebug) {
+    Serial.begin(9600);
+
+    Serial.print("Star Wars\n");
+  }
+
   pinMode(kBuzzerPin, OUTPUT);
 
   for (int i = 0; i < kLedPinCount; ++i)
@@ -17,78 +25,131 @@ void setup()
   }
 }
 
-void playNote(int note, int duration)
+void playNote(int buzzerPin, int note, int duration)
 {
   int ledPin = kFirstLedPin + (noteIndex % kLedPinCount);
 
   if (note)
   {
     digitalWrite(ledPin, HIGH);
-    tone(kBuzzerPin, note, duration);
+    tone(buzzerPin, note, duration * 25);
   }
 
-  delay(duration);
+  delay(duration * 25);
 
   if (note)
   {
     digitalWrite(ledPin, LOW);
 
-    delay(50);
+    delay(2 * 25);
     ++noteIndex;
   }
 }
 
+void playSong(int buzzerPin, const char *song)
+{
+  int head = 0;
+  unsigned int len = strlen(song);
+
+  while (head < len)
+  {
+    int err = 0;
+
+    int note = 0;
+    int sharp = 0;
+    int octave = 0;
+    int duration = 0;
+
+    char c = ' ';
+    while (head < len && (c == ',' || c == ' ')) c = song[head++];
+
+    if (c >= 'A' && c <= 'G')
+    {
+      note = c - 'A';
+      c = song[head++];
+      if (c == '#')
+      {
+        sharp = 1;
+        c = song[head++];
+      }
+      if (c >= '0' && c <= '9')
+      {
+        octave = c - '0';
+        c = song[head++];
+      }
+    }
+
+    if (c == '-')
+    {
+      while ((c = song[head++]) && c != ',' && err == 0)
+      {
+          if (c >= '0' && c <= '9')
+          {
+            duration = (duration * 10) + c - '0';
+          }
+          else err = 1;
+      }
+    } else err = 1;
+
+    if (err == 0)
+    {
+      //                           A    A#   B   -   C    C#   D    D#   E   -   F    F#   G    G#
+      int frequenciesTable[] = {  220, 233, 247, 0, 261, 277, 293, 311, 329, 0, 349, 370, 392, 415, // 3
+                                  440, 466, 494, 0, 523, 554, 587, 622, 659, 0, 698, 740, 784, 830, // 4
+                                  880, 932, 988, 0 };
+
+     int frequency = 0;
+     if (octave >= 3 && octave <= 5)
+     {
+      int index = (octave - 3) * 14 + note * 2 + sharp;
+      frequency = frequenciesTable[index];
+     }
+          
+      playNote(buzzerPin, frequency, duration);
+
+      if (kDebug)
+      {
+        char sNote[4];
+        int k = 0;
+        sNote[k++] = 'A' + note;
+        if (sharp == 1)
+          sNote[k++] = '#';
+        sNote[k++] = '0' + octave;
+        sNote[k] = 0x00;
+
+        Serial.print(sNote);
+        Serial.print("-");
+        Serial.print(duration);
+        Serial.print("\n");
+      }
+    }
+    else
+    {
+      if (kDebug)
+      {
+        char preview[5];
+        strncpy((char *)&preview, (const char *)&song[head], 4);
+        preview[4] = 0x00;
+
+        Serial.print("Wrong note! Error in: '");
+        Serial.print(preview);
+        Serial.print("'\n");
+      }
+    }
+  }
+}
+
+const char kStarWarsSong[] = 
+    "A4-20, A4-20, A4-20, F3-14, C4-6, A4-20, F3-14, C4-6, A4-26, -20, "
+    "E4-20, E4-20, E4-20, F4-14, C4-6, G#3-20, F3-14, C4-6, A4-26, -20, "
+    "A5-20, A4-12, A4-6, A5-20, G#4-13, G4-7, F#4-5, F4-5, F#4-10, -13, "
+    "A#4-10, D#4-20, D4-13, C#4-7, C4-5, B4-5, C4-10, -14, "
+    "F3-10, G#3-20, F3-14, A4-5, C4-20, A4-15, C4-5, E4-26, -20"
+    "";
+
 void loop()
 {
-  playNote(440, 20 * 25); // A4
-  playNote(440, 20 * 25); // A4
-  playNote(440, 20 * 25); // A4
-  playNote(349, 14 * 25); // F4
-  playNote(523, 6 * 25); // C5
-  playNote(440, 20 * 25); // A4
-  playNote(349, 14 * 25); // F4
-  playNote(523, 6 * 25); // C5
-  playNote(440, 26 * 25); // A4
-  playNote(0, 20 * 25); // -
- 
-  playNote(659, 20 * 25); // E5
-  playNote(659, 20 * 25); // E5
-  playNote(659, 20 * 25); // E5
-  playNote(698, 14 * 25); // F5
-  playNote(523, 6 * 25); // C5
-  playNote(415, 20 * 25); // G#4
-  playNote(349, 14 * 25); // F4
-  playNote(523, 6 * 25); // C5
-  playNote(440, 26 * 25); // A4
-  playNote(0, 20 * 25); // -
 
-  playNote(880, 20 * 25); // A5
-  playNote(440, 12 * 25); // A4
-  playNote(440, 6 * 25); // A4
-  playNote(880, 20 * 25); // A5
-  playNote(830, 13 * 25); // G#5
-  playNote(784, 7 * 25); // G5
-  playNote(740, 5 * 25); // F#5
-  playNote(698, 5 * 25); // F5
-  playNote(740, 10 * 25); // F#5
-  playNote(0, 13 * 25); // -
- 
-  playNote(466, 10 * 25); // A#4
-  playNote(622, 20 * 25); // D#5
-  playNote(587, 13 * 25); // D5
-  playNote(554, 7 * 25); // C#5
-  playNote(523, 5 * 25); // C5
-  playNote(482, 5 * 25); // B4
-  playNote(523, 10 * 25); // C5
-  playNote(0, 14 * 25); // -
-  
-  playNote(349, 10 * 25); // F4
-  playNote(415, 20 * 25); // G#4
-  playNote(349, 14 * 25); // F4
-  playNote(440, 5 * 25); // A4
-  playNote(523, 20 * 25); // C5
-  playNote(440, 15 * 25); // A4
-  playNote(523, 5 * 25); // C5
-  playNote(659, 26 * 25); // E5
-  playNote(0, 20 * 25); // -
- }
+  playSong(kBuzzerPin, kStarWarsSong);
+  delay(1000);
+}
