8447262e51320a20dc285c5be335286dc77cd6fd
Thomas CARTON <tom.carton@gmail.com>
2021-10-04 08:54:37 +0200

    Set durations eith multiples of 25

--- Arduino/StarWars/StarWars.ino
+++ Arduino/StarWars/StarWars.ino
@@ -32,60 +32,60 @@ void playNote(int note, int duration)
 
 void loop()
 {
-  playNote(440, 10 * 50); // A4
-  playNote(440, 10 * 50); // A4
-  playNote(440, 10 * 50); // A4
-  playNote(349, 7 * 50); // F4
-  playNote(523, 3 * 50); // C5
-  playNote(440, 10 * 50); // A4
-  playNote(349, 7 * 50); // F4
-  playNote(523, 3 * 50); // C5
-  playNote(440, 13 * 50); // A4
+  playNote(440, 20 * 25); // A4
+  playNote(440, 20 * 25); // A4
+  playNote(440, 20 * 25); // A4
+  playNote(349, 14 * 25); // F4
+  playNote(523, 6 * 25); // C5
+  playNote(440, 20 * 25); // A4
+  playNote(349, 14 * 25); // F4
+  playNote(523, 6 * 25); // C5
+  playNote(440, 26 * 25); // A4
  
   delay(500);
  
-  playNote(659, 10 * 50); // E5
-  playNote(659, 10 * 50); // E5
-  playNote(659, 10 * 50); // E5
-  playNote(698, 7 * 50); // F5
-  playNote(523, 3 * 50); // C5
-  playNote(415, 10 * 50); // G#4
-  playNote(349, 7 * 50); // F4
-  playNote(523, 3 * 50); // C5
-  playNote(440, 13 * 50); // A4
+  playNote(659, 20 * 25); // E5
+  playNote(659, 20 * 25); // E5
+  playNote(659, 20 * 25); // E5
+  playNote(698, 14 * 25); // F5
+  playNote(523, 6 * 25); // C5
+  playNote(415, 20 * 25); // G#4
+  playNote(349, 14 * 25); // F4
+  playNote(523, 6 * 25); // C5
+  playNote(440, 26 * 25); // A4
  
   delay(500);
 
-  playNote(880, 500); // A5
-  playNote(440, 300); // A4
-  playNote(440, 150); // A4
-  playNote(880, 500); // A5
-  playNote(830, 325); // G#5
-  playNote(784, 175);  // G5
-  playNote(740, 125); // F#5
-  playNote(698, 125);  // F5
-  playNote(740, 250); // F#5
+  playNote(880, 20 * 25); // A5
+  playNote(440, 12 * 25); // A4
+  playNote(440, 6 * 25); // A4
+  playNote(880, 20 * 25); // A5
+  playNote(830, 13 * 25); // G#5
+  playNote(784, 7 * 25); // G5
+  playNote(740, 5 * 25); // F#5
+  playNote(698, 5 * 25); // F5
+  playNote(740, 10 * 25); // F#5
  
   delay(325);
  
-  playNote(466, 250); // A#4
-  playNote(622, 500); // D#5
-  playNote(587, 325); // D5
-  playNote(554, 175); // C#5
-  playNote(523, 125); // C5
-  playNote(482, 125); // B4
-  playNote(523, 250); // C5
+  playNote(466, 10 * 25); // A#4
+  playNote(622, 20 * 25); // D#5
+  playNote(587, 13 * 25); // D5
+  playNote(554, 7 * 25); // C#5
+  playNote(523, 5 * 25); // C5
+  playNote(482, 5 * 25); // B4
+  playNote(523, 10 * 25); // C5
  
   delay(350);
   
-  playNote(349, 250); // F4
-  playNote(415, 500); // G#4
-  playNote(349, 350); // F4
-  playNote(440, 125); // A4
-  playNote(523, 500); // C5
-  playNote(440, 375); // A4
-  playNote(523, 125); // C5
-  playNote(659, 650); // E5
+  playNote(349, 10 * 25); // F4
+  playNote(415, 20 * 25); // G#4
+  playNote(349, 14 * 25); // F4
+  playNote(440, 5 * 25); // A4
+  playNote(523, 20 * 25); // C5
+  playNote(440, 15 * 25); // A4
+  playNote(523, 5 * 25); // C5
+  playNote(659, 26 * 25); // E5
  
   delay(500);
 }
